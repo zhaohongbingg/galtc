a7bcd3a730e72c54e6e50e616c1c54ce55530ce5
RIDRESS <56878262+zhaohongbingg@users.noreply.github.com>
2025-11-26 15:53:14 +0800

    Delete src/main/java/modifiers directory

--- src/main/java/modifiers/materid.java
+++
@@ -1,8 +0,0 @@
-package modifiers;
-
-import slimeknights.tconstruct.library.materials.definition.MaterialId;
-
-public class materid {
-    public static final MaterialId SPRB =new MaterialId("galtc2:sprb");
-
-}
